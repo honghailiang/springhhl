30e37a72c2b874e1805a04b097a4d97ca92ac1b8
honghailiang <honghailaing666@126.com>
2019-01-03 16:44:04 +0800

    api gateway filter

--- api-gateway/src/main/java/com/jtt/hhl/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jtt/hhl/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.jtt.hhl;
 
+import com.jtt.hhl.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 
 }
---
+++ api-gateway/src/main/java/com/jtt/hhl/filter/AccessFilter.java
@@ -0,0 +1,51 @@
+package com.jtt.hhl.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description: java类作用描述
+ * @Author: Herman
+ * @CreateDate: 2019/1/3 16:31
+ */
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest httpServletRequest = requestContext.getRequest();
+        logger.info("send {} request to{}", httpServletRequest.getMethod(), httpServletRequest.getRequestURL().toString());
+        Object accessToken = httpServletRequest.getParameter("accessToken");
+        if (accessToken == null) {
+            logger.warn("access token is empty");
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            return null;
+        }
+
+        logger.info("access token ok");
+        return null;
+    }
+}
